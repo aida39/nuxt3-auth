bf38e9474c8108b5c30bdf0d49b2e56cc95951cb
aida <alma2013mtk@gmail.com>
2024-07-29 13:50:49 +0900

    4.認証設定まで学習完了

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <nav>
+            <ul>
+                <li>
+                    <NuxtLink to="/">Home</NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="/login">Login</NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="/dashboard">Dashboard</NuxtLink>
+                </li>
+            </ul>
+        </nav>
+        <slot />
+    </div>
+</template>
